244a24555daf5862d61ecbd614f818ce45b954ca
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2022-10-26 16:49:48 +0200

    Inizio NativeApp

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+      body: //QUI CONSUMO Places
+          const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
